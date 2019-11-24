174dc8fd5e321536161f668ce8e2a4edc80d9b21
Otabek0626 <olimjonovotabek0@users.noreply.github.com>
2019-11-24 15:11:04 +0900

    Create 004.py

---
+++ The Basics/004.py
@@ -0,0 +1,7 @@
+#Ask the user to enter two numbers. Add them together and
+#display the answer as
+#The total is [answer].
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+answer = num1 + num2
+print("The total is",answer)
