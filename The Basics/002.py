f0602d750edfc232bcf8a5b5f330a55b18f85759
Otabek0626 <olimjonovotabek0@users.noreply.github.com>
2019-11-24 15:10:52 +0900

    Create 002.py

---
+++ The Basics/002.py
@@ -0,0 +1,6 @@
+#Ask for the user’s first name and then ask for
+#their surname and display the output message
+#Hello [First Name] [Surname] .
+first_name = input("Enter your first name: ")
+sur_name = input("Enter your sur name: ")
+print("Hello",first_name,sur_name)
