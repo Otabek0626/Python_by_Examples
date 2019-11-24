1f5c9f3e938016342aff9e494b5ba4e1b323afa9
Otabek0626 <olimjonovotabek0@users.noreply.github.com>
2019-11-24 15:11:18 +0900

    Create 010.py

---
+++ The Basics/010.py
@@ -0,0 +1,6 @@
+#There are 2,204 pounds in a kilogram. Ask the
+#user to enter a weight in kilograms and convert it
+#to pounds.
+kilo = int(input("How many kilos? "))
+pound = kilo * 2.204
+print(f'That is {pound} pounds') #I used f'bla {bla} bla' it saves our time
